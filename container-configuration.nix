51f0323b13b4f0ecf7fc98b86e56adb4543c2ec7
Bud-ro <gamercarsonzero@gmail.com>
2022-03-17 22:45:14 -0400

    Update container-configuration.nix
    Fixed naming issue

--- container-configuration.nix
+++ container-configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
   {
     config.virtualisation.oci-containers.containers = {
-      changedetection.io = { # will this work when there's a . in the name?
+      changedetection-io = {
         image = "dgtlmoon/changedetection.io:latest";
         ports = ["127.0.0.1:5000:5000"];
         volumes = [
